8d09d4688fd461b831de5a1b64b5c59b77f4c5a3
Rendszergazda <rendszergazda@blathy.info>
2026-03-26 12:28:37 +0100

    ongoing

--- vizsgaremek/frontend/src/utils/http.js
+++ vizsgaremek/frontend/src/utils/http.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+/*import axios from "axios";
 
 export const http = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:5173",
     headers:{
         "Content-Type": "application/json"
     }
-})
+})*/
